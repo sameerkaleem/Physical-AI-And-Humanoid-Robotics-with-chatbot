c7154888316fddcc4082a3551ca5e5c942040dbb
sameerkaleem <sameerkaleem@gmail.com>
2025-12-19 16:54:37 +0500

    book added

---
+++ book/.docusaurus/client-modules.js
@@ -0,0 +1,7 @@
+export default [
+  require("C:\\Users\\HP\\Desktop\\textbook\\book\\.docusaurus\\docusaurus-plugin-css-cascade-layers\\default\\layers.css"),
+  require("C:\\Users\\HP\\Desktop\\textbook\\book\\node_modules\\infima\\dist\\css\\default\\default.css"),
+  require("C:\\Users\\HP\\Desktop\\textbook\\book\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
+  require("C:\\Users\\HP\\Desktop\\textbook\\book\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
+  require("C:\\Users\\HP\\Desktop\\textbook\\book\\src\\css\\custom.css"),
+];
